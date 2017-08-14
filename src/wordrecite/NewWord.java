cdbbc5832f7c319776a1f0c1bf7f8d1683f1e765
allan <fierliu@126.com>
2017-08-14 16:32:05 +0800

    2nd commit.

--- src/wordrecite/NewWord.java
+++ src/wordrecite/NewWord.java
@@ -17,11 +17,10 @@ import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
-public class NewWord extends JFrame implements ActionListener{
+public class NewWord {
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = 1L;
 	ReadDailyNum rdn = new ReadDailyNum();
 	private  int dailyNum = rdn.readDailyNum();	
 	WordHandler readl = new WordHandler();
@@ -116,136 +115,120 @@ public class NewWord extends JFrame implements ActionListener{
 		
 		jf.add(jlMenu);
 		jf.setJMenuBar(jmb);
-		
-		
-		
+				
 		//jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);关闭时应先执行WritFile
 		jf.setSize(1200, 800);
 		jf.setResizable(false);
 		jf.setLocation(300, 100);	
 		jf.setVisible(true);	
 	}
-		//获取hide标签的值
-		public int getHideNum() throws NumberFormatException{			
-			    int index = Integer.parseInt(hide.getText());
-				return index;				
-		}
+	//获取hide标签的值
+	public int getHideNum() throws NumberFormatException{			
+		    int index = Integer.parseInt(hide.getText());
+			return index;				
+	}
 		
-		public void showNextWord(int i){
-//			if (i == -1) JOptionPane.showMessageDialog(jf, "词库不存在，请导入新词库");
-//			else{
-				learned.setText("本次学习"+ dailyNum+ "个，已学习"+ count+ "个，");
-				word.setText(readl.showWord(i));
-				hide.setText(String.valueOf(i));//将随机索引值记录在hide标签里
-				explain.setText(null);
-				
-				if (readl.getWritelistSize() == 0){
-					JOptionPane.showMessageDialog(jf, "本词库学习已结束，请导入新词库");//循环结束，弹窗显示已学完	
-//				}
+	public void showNextWord(int i){
+			learned.setText("本次学习"+ dailyNum+ "个，已学习"+ count+ "个，");
+			word.setText(readl.showWord(i));
+			hide.setText(String.valueOf(i));//将随机索引值记录在hide标签里
+			explain.setText(null);
+			
+			if (readl.getWritelistSize() == 0){
+				JOptionPane.showMessageDialog(jf, "本词库学习已结束，请导入新词库");//循环结束，弹窗显示已学完	
 			}	
-		}
+	}
 		
 /*********************************************ActionListener setted by inner class***************************************/
-		private class NextButtonHandler implements ActionListener{//“下一个”键监听
-			public void actionPerformed(ActionEvent e){				
-				int i = readl.makeRandom();
-//				System.out.println("random = "+ i);
-				
-				if(count> dailyNum ){
-					JOptionPane.showMessageDialog(jf, "本次学习已结束");//循环结束，弹窗显示已学完	
-					try {
-						readl.writefile();
-					} catch (UnsupportedEncodingException | FileNotFoundException e1) {
-						e1.printStackTrace();
-					}
+	private class NextButtonHandler implements ActionListener{//“下一个”键监听
+		public void actionPerformed(ActionEvent e){				
+			int i = readl.makeRandom();
+//				System.out.println("random = "+ i);				
+			if(count> dailyNum ){
+				JOptionPane.showMessageDialog(jf, "本次学习已结束");//循环结束，弹窗显示已学完	
+				try {
+					readl.writefile();
+				} catch (UnsupportedEncodingException | FileNotFoundException e1) {
+					e1.printStackTrace();
 				}
-				
-				if(readl.getWritelistSize()<dailyNum){
-					i = readl.makeLowRandom();
-				}
-				
-				else{
-					showNextWord(i);
-						count += 1;
-				}
-			}
-		}
-		
-		private class AccomplishButtonHandler implements ActionListener{//“已记住”键的监听
-			public void actionPerformed(ActionEvent e){
-				if(count> dailyNum )
-					JOptionPane.showMessageDialog(jf, "本次学习已结束");//循环结束，弹窗显示已学完					
-				else{					
-					readl.delword(getHideNum());
-//					System.out.println("del index"+ hide.getText());
-					total.setText("共"+ readl.getWritelistSize()+"个");					
-					
-					int i = readl.makeRandom();
-					showNextWord(i);
-	
+			}				
+			if(readl.getWritelistSize()<dailyNum){
+				i = readl.makeLowRandom();
+			}				
+			else{
+				showNextWord(i);
 					count += 1;
-				}
 			}
 		}
-		//“释意”键
-		private class ParaphraseButtonHandler implements ActionListener{
-			public void actionPerformed(ActionEvent e){
-//				System.out.println("explain index"+ hide.getText());
-			    explain.setText(readl.showExplain(getHideNum()));
-			}
-		}
-//		打开文件
-		private class OpenFile implements ActionListener{
-			public void actionPerformed(ActionEvent e){
-				FileDialog open = new FileDialog(jf, "打开文件",FileDialog.LOAD);
-				open.show();
-				String fileName = open.getDirectory() + open.getFile();
-				if (fileName != null) {
-					try {
-						new Copy(fileName);//先这样，不行再把copy中的方法从构造函数中移出去
-						readl = new WordHandler();
-						total.setText("共"+ readl.getTotalSize()+"个"); 
-						word.setText(null);
-//						new NewWord();
-						JOptionPane.showMessageDialog(jf, "导入成功！请按\"下一个\"继续");
-					} catch (IOException e1) {
-						e1.printStackTrace();
-					}
-				}
-			}
-		}
-//		设置学习量
-		private class SetDaily implements ActionListener{
-			public void actionPerformed(ActionEvent e){
-				JFrame jfDaily = new JFrame("设置日学习量");
-				JTextField jtfDaily = new JTextField(3);
-				JLabel jlDaily = new JLabel("请输入日学习数量,按Enter确认：");
-				FlowLayout  flow=new FlowLayout( );
-//				jfDaily.setContentPane(jp); 
-				jfDaily.setLayout(flow);
-				jfDaily.add(jlDaily);
-				jfDaily.add(jtfDaily);
-				jfDaily.pack();
-				jfDaily.setBounds(800, 500, 400, 100);
-				jfDaily.setVisible(true);
-				jtfDaily.addActionListener(new ActionListener(){//输入新的学习量后的动作
-					public void actionPerformed(ActionEvent e) {
-						rdn.writeDailyNum(jtfDaily.getText());
-						JOptionPane.showMessageDialog(jfDaily, "设置成功，下次启动程序时生效！");
-						jfDaily.dispose();//关闭jframe jfDaily.
-					}
-				});
+	}
+	//“已记住”键的监听
+	private class AccomplishButtonHandler implements ActionListener{
+		public void actionPerformed(ActionEvent e){
+			if(count> dailyNum )
+				JOptionPane.showMessageDialog(jf, "本次学习已结束");//循环结束，弹窗显示已学完					
+			else{					
+				readl.delword(getHideNum());
+//					System.out.println("del index"+ hide.getText());
+				total.setText("共"+ readl.getWritelistSize()+"个");					
 				
+				int i = readl.makeRandom();
+				showNextWord(i);
+
+				count += 1;
 			}
-		}	
+		}
+	}
+	//“释意”键
+	private class ParaphraseButtonHandler implements ActionListener{
+		public void actionPerformed(ActionEvent e){
+//				System.out.println("explain index"+ hide.getText());
+		    explain.setText(readl.showExplain(getHideNum()));
+		}
+	}
+//		打开文件
+	private class OpenFile implements ActionListener{
+		public void actionPerformed(ActionEvent e){
+			FileDialog open = new FileDialog(jf, "打开文件",FileDialog.LOAD);
+			open.show();
+			String fileName = open.getDirectory() + open.getFile();
+			if (fileName != null) {
+				try {
+					new Copy(fileName);
+					readl = new WordHandler();
+					total.setText("共"+ readl.getTotalSize()+"个"); 
+					word.setText(null);
+//						new NewWord();
+					JOptionPane.showMessageDialog(jf, "导入成功！请按\"下一个\"继续");
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+			}
+		}
+	}
+//		设置学习量
+	private class SetDaily implements ActionListener{
+		public void actionPerformed(ActionEvent e){
+			JFrame jfDaily = new JFrame("设置日学习量");
+			JTextField jtfDaily = new JTextField(3);
+			JLabel jlDaily = new JLabel("请输入日学习数量,按Enter确认：");
+			FlowLayout  flow=new FlowLayout( );
+			jfDaily.setLayout(flow);
+			jfDaily.add(jlDaily);
+			jfDaily.add(jtfDaily);
+			jfDaily.pack();
+			jfDaily.setBounds(800, 500, 400, 100);
+			jfDaily.setVisible(true);
+			jtfDaily.addActionListener(new ActionListener(){//输入新的学习量后的动作
+				public void actionPerformed(ActionEvent e) {
+					rdn.writeDailyNum(jtfDaily.getText());
+					JOptionPane.showMessageDialog(jfDaily, "设置成功，下次启动程序时生效！");
+					jfDaily.dispose();//关闭jframe jfDaily.
+				}
+			});				
+		}
+	}	
 
 	public static void main(String[] args) {
 		new NewWord();
 	}
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
 }
